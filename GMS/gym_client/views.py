c20a1cb0eb73a43cd6ed2310ba76b05c62b687d2
Sudin Rajbhandari <sudin.rajbandari@deerwalk.edu.np>
2023-08-07 10:22:26 +0545

    initial_commit

--- GMS/gym_client/views.py
+++ GMS/gym_client/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 
 def gym_client(request):
     print("Gym Client View function executed.")
-    return HttpResponse("Client Page")
+    template=loader.get_template('gym_client_index.html')
+    return HttpResponse(template.render())
